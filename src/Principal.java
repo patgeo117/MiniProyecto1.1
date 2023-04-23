8f0b60f813bb3d9d5d867c9f25f013c348301208
patgeo117 <gpatino017@gmail.com>
2023-04-23 16:35:36 -0500

    Iniciador

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 
 public class Principal extends JFrame {
     JFrame VenPricipal,VenReportes;
+    public Principal(){
+        VenPricipal.setSize(500,500); // Tamaño de la ventana
+        VenPricipal.setTitle("CAFETERIA"); // Titulo de la ventana
+    }
 
     public static void main(String[] args) {
 
