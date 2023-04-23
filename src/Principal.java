da9914b06b43428c408234c8b45391c53db3fa98
Johan <johanserna842@gmail.com>
2023-04-23 16:16:46 -0500

    Hola

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Principal extends JFrame {
     JFrame VenPricipal,VenReportes;
 
-    // Bobo HPTA
+    // Bobo HPTA//
     public static void main(String[] args) {
 
 
