8182e45017927d9776efcc3d48e512a36c41d31e
patgeo117 <gpatino017@gmail.com>
2023-04-23 17:01:53 -0500

    Iniciador

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Principal extends JFrame {
-    JFrame VenPricipal,VenReportes;
+public class Principal {
+    JFrame VenPricipal = new JFrame();
+    JFrame VenReportes = new JFrame();
+
     public Principal(){
-        VenPricipal.setSize(500,500); // Tamaño de la ventana
+
+        VenPricipal.setLayout(null);
+        VenPricipal.setSize(600,600); // Tamaño de la ventana
         VenPricipal.setTitle("CAFETERIA"); // Titulo de la ventana
+        VenPricipal.setLocationRelativeTo(null); // posicion de la ventana en el centro
+        VenPricipal.setResizable(false); // No cambiar de tamaño la ventana
+        VenPricipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Cerrar ventana
+        VenPricipal.setVisible(true);
+
+        IniciarComponetes();
+    }
+    private void IniciarComponetes(){
+
+        //Botones
+        //Labels
+        //Paneles ETC
+
+
     }
 
     public static void main(String[] args) {
-
+        new Principal();
 
     }
 }
