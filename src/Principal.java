b212ae8787bc74b0b09474a6e6ce35e82d30df07
patgeo117 <gpatino017@gmail.com>
2023-04-24 15:32:29 -0500

    ComboBox

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
-import java.awt.*;
 import java.util.Objects;
 
 public class Principal {
     JFrame VenPricipal = new JFrame();
     ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Iconos/LogoCafeP.png")));
+    int s = Integer.parseInt((String) JOptionPane.showInputDialog(null,"Ingresa número de mesas a utilizar: ","Cafeteria", JOptionPane.QUESTION_MESSAGE, img, null, ""));
     JFrame VenReportes = new JFrame();
+    JComboBox<String> mesas = new JComboBox<String>() ;
+    JComboBox<String> listProduct;
+
+    String[] productos = {"Gaseosa", "Juego", "Papas fritas", "Capuchinnos", "Americano"};
+
 
     public Principal(){
 
-        int s = Integer.parseInt((String) JOptionPane.showInputDialog(null,"Ingresa número de mesas a utilizar: ","Cafeteria", JOptionPane.QUESTION_MESSAGE, img, null, ""));
+
         VenPricipal.setLayout(null);
         VenPricipal.setSize(600,600); // Tamaño de la ventana
         VenPricipal.setTitle("CAFETERIA"); // Titulo de la ventana
@@ -28,6 +33,18 @@ public class Principal {
         IniciarComponetes();
     }
     private void IniciarComponetes(){
+        //ComboBox
+        mesas.setBounds(10,10,80,60);
+
+        for (int i = 1; i <= s; i++) {
+            mesas.addItem("Mesa " + i);
+        }
+        VenPricipal.add(mesas);
+
+        //ComboBox
+        listProduct = new JComboBox<>(productos);
+        listProduct.setBounds(100,10,80,60 );
+        VenPricipal.add(listProduct);
 
         //Botones
         //Labels
