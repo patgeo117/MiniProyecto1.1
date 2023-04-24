f565f9dd61c7f8e94cff88f0418eb1102c6a5819
Johan <johanserna842@gmail.com>
2023-04-24 14:43:45 -0500

    Hola

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
 
 public class Principal {
     JFrame VenPricipal = new JFrame();
-    ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource("/Iconos/LogoCafeP.png")));
+    ImageIcon img = new ImageIcon(getClass().getResource("/Iconos/LogoCafeP.png"));
     JFrame VenReportes = new JFrame();
 
     public Principal(){
+
+        int s = Integer.parseInt((String) JOptionPane.showInputDialog(null,"Ingresa número de mesas a utilizar: ","Cafeteria", JOptionPane.QUESTION_MESSAGE, img, null, ""));
         VenPricipal.setLayout(null);
         VenPricipal.setSize(600,600); // Tamaño de la ventana
         VenPricipal.setTitle("CAFETERIA"); // Titulo de la ventana
         VenPricipal.setLocationRelativeTo(null); // posicion de la ventana en el centro
         VenPricipal.setResizable(false); // No cambiar de tamaño la ventana
         VenPricipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Cerrar ventana
-        VenPricipal.setVisible(true); // Se muestra la Ventana
         VenPricipal.setIconImage(img.getImage());
+
+        if(s>0){
+            VenPricipal.setVisible(true); // Se muestra la Ventana
+        }
 
         IniciarComponetes();
     }
@@ -29,11 +33,6 @@ public class Principal {
     }
 
     public static void main(String[] args) {
-        int s = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresa número de mesas a utilizar: ","Cafeteria", JOptionPane.PLAIN_MESSAGE));
-
-        if (s>0){
             new Principal();
-        }
-
     }
 }
