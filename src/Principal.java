7b14a87bd01a6b3c9b47292a2b3c30f3c28e85aa
patgeo117 <gpatino017@gmail.com>
2023-04-24 14:32:39 -0500

    Iniciador

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class Principal {
     JFrame VenPricipal = new JFrame();
+    ImageIcon img = new ImageIcon(getClass().getResource("/Iconos/LogoCafeP.png"));
     JFrame VenReportes = new JFrame();
 
     public Principal(){
-
         VenPricipal.setLayout(null);
         VenPricipal.setSize(600,600); // Tamaño de la ventana
         VenPricipal.setTitle("CAFETERIA"); // Titulo de la ventana
@@ -12,6 +14,7 @@ public class Principal {
         VenPricipal.setResizable(false); // No cambiar de tamaño la ventana
         VenPricipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Cerrar ventana
         VenPricipal.setVisible(true); // Se muestra la Ventana
+        VenPricipal.setIconImage(img.getImage());
 
         IniciarComponetes();
     }
